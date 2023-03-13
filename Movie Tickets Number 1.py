13efeef74a6e1c4f24063edc000d59e6f1e966c3
Qiaos1 <qiaos@middleton.school.nz>
2023-03-14 10:12:35 +1300

    Hi

--- Movie Tickets Number 1.py
+++ Movie Tickets Number 1.py
@@ -1,1 +1,20 @@
-print("Hello")
+"""Movie Theater Ticketing System - v1
+Welcome system and variables
+Created by Sarah Qiao
+"""
+
+
+# Component 1 - Welcoming screen and set up variables
+def sell_ticket():
+    print("********** Fanfare Movies - ticketing system **********\n")
+
+    adult_tickets = 0
+    student_tickets = 0
+    child_tickets = 0
+    gift_tickets = 0
+    tickets_sold = 0
+    total_sold = 0
+
+
+# Main routine
+sell_ticket()
